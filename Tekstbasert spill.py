b9d12d3b6417f40b37c05789b83f8a763798f18a
mse2004 <118967095+mse2004@users.noreply.github.com>
2022-12-01 09:51:57 +0100

    Update Tekstbasert spill.py

--- Tekstbasert spill.py
+++ Tekstbasert spill.py
@@ -1,3 +1,23 @@
 class Person:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, valg1, valg2, valg3) -> None:
+        self.valg1 = valg1
+        self.valg2 = valg2
+        self.valg3 = valg3
+
+class Olivia(Person):
+    def __init__(self, valg1, valg2, valg3) -> None:
+        self.valg1 = valg1
+        self.valg2 = valg2
+        self.valg3 = valg3
+
+class Brian(Person):
+    def __init__(self, valg1, valg2, valg3) -> None:
+        self.valg1 = valg1
+        self.valg2 = valg2
+        self.valg3 = valg3
+
+class Kat(Person):
+    def __init__(self, valg1, valg2, valg3) -> None:
+        self.valg1 = valg1
+        self.valg2 = valg2
+        self.valg3 = valg3
