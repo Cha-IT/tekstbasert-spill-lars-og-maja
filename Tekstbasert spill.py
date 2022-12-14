71bb85b48e9ef04ebaf5ec57f7c066f7fb6bd14a
mse2004 <118967095+mse2004@users.noreply.github.com>
2022-12-14 15:23:28 +0100

    Update Tekstbasert spill.py

--- Tekstbasert spill.py
+++ Tekstbasert spill.py
@@ -82,11 +82,18 @@ class Olivia(Person):
 
     def braEnding(self):
         print("")
+        print("(Det er nå to uker senere, og du og Olivia har blitt mye bedre kjent. En dag sitter dere og snakker hjemme hos henne.")
+        print("Olivia: Du, vi passer egentlig ganske godt sammen, har du tenkt over det?")
+        print(f"{spillerNavn}: Ja, det må jeg si meg enig i.")
+        print("(Du smiler til henne. Hun smiler tilbake)")
+        print("(Ett år senere er dere fortsatt sammen, og begge går på NTNU. Olivia studerer litteratur, og ønsker å bli enda bedre enn helten hennes Lars Larsson.)")
         print("")
         print("Du fikk ending 1, den brae endingen til Olivia, gratulerer! Det er totalt 6 forskjellige endings i spillet.")
 
     def dårligEnding(self):
-        print(":(")
+        print("")
+        print("(Olivia likte deg fra første gang dere møttes, men var redd for å si noe)")
+        print("(Etter at du og Olivia ikke kom godt overens, var Olivia helt knust. Hun droppet ut av skolen rett før slutten av vg3, og bor i dag i kjelleren til moren, uten å ha oppnådd noe i livet.)")
         print("")
         print("Du fikk ending 2, den dårlige endingen til Olivia. Det er totalt 6 forskjellige endings i spillet.")
 
@@ -145,10 +152,13 @@ class Kat(Person):
         print(f"{spillerNavn}: Oi, er det noe viktig?")
         print("Kat: Hva er best, cola eller pepsi?")
         brusValg = input()
-        if (brusValg == "Pepsi" or "pepsi") or (brusValg == "Monster" or "monster"):
+        if (brusValg == "Pepsi" or brusValg == "pepsi"):
             self.valg4 = True
             print("Kat: Niceee, helt enig.")
-        elif brusValg == "Cola" or "cola":
+        elif (brusValg == "Monster" or brusValg == "monster"):
+            self.valg4 = True
+            print("Kat: Det var egentlig ikke et alternativ, men er likevel heelt enig.")
+        elif brusValg == "Cola" or brusValg == "cola":
             self.valg4 = False
             print("Kat: Nei æsj hva er galt med deg.")
         else:
@@ -157,9 +167,21 @@ class Kat(Person):
 
     def braEnding(self):
         print("")
+        print("(Etter en uke er du og Kat blitt veldig nære venner. Dere er hjemme hos deg og spiller Minecraft sammen.")
+        print("(Kat snur seg mot deg og kysser deg. Du blir overrasket, men kysser hun tilbake.)")
+        print("(Ett år senere er dere fortsatt sammen. Kat ble proff esportspiller og har allerede tjent store premiepenger i forskjellige konkurranser.)")
+        print("")
         print("Du fikk ending 3, den brae endingen til Kat, gratulerer! Det er totalt 6 forskjellige endings i spillet.")
 
     def dårligEnding(self):
+        print("")
+        print("(Du og Kat har gått dårlig overens den første skoledagen på den nye skolen. Dagen etter finner du en lapp i skapet ditt.)")
+        print("Lapp: Finn meg bak skolen ved søppeldunken kl 14.00. -Kat")
+        print("(Du gjør som lappen sa, og ser Kat stå på det bestemte møtestedet deres.)")
+        print("Kat: Hvis det er en ting du bør vite om meg, er det at jeg ikke liker når andre ikke har respekt for meg. Det er flaut å bli sett med deg.")
+        print(f"{spillerNavn}: Sier du? Du er jo kjent på skolen for å være en skikkelig raring??")
+        print("(Fjeset til Kat blir rødt av sinne. Hun brøler noen banneord du ikke vet hva betyr engang. De virker alvorlige.)")
+        print("(Kat vokser og vokser, helt til hun dekker til solen. Hun har blitt om til et monster. Du hyler, men det er for sent. Hun spiser deg hel.)")
         print("")
         print("Du fikk ending 4, den dårlige endingen til Kat. Det er totalt 6 forskjellige endings i spillet.")
 
@@ -191,8 +213,8 @@ class Brian(Person):
             print("")
         elif valg2 == "frekk":
             self.valg2 = False
-            print(f"{spillerNavn}: Halla kæm e du. Koffor går du rundt med så rare klær")
-            print(f"Brian: Æ hete Brian kæm tror du du e? Snakke om mine klær mens du går rundt som en jævla uteligger. Lukte dårlig og")
+            print(f"{spillerNavn}: Halla kæm e du. Koffor går du rundt med så rare klær og den jalla røde kapsen")
+            print(f"Brian: Æ hete Brian kæm tror du at du e? Snakke om mine klær mens du går rundt som en jævla uteligger. Lukte dårlig og")
             print("Brian: Du e en jævla nisse komm dæ ut av ansiktet mitt")
             print("(Du går vekk)")
             print("")
@@ -204,7 +226,7 @@ class Brian(Person):
         filmValg1 = input()
         print("Aaah ok. Har du sett interstellar da? Liker du den? ")
         filmValg2 = input()
-        if filmValg2 == "Ja" or "ja":
+        if filmValg2 == "Ja" or filmValg2 == "ja":
             print("Herlig det er favorittfilmen min")
         else:
             print("Ok skjønner")
@@ -222,13 +244,39 @@ class Brian(Person):
             self.valg3 = False
     
     def sisteSpørsmål(self):
-        return
+        print("")
+        print("Brian: Yo kiz æ har et viktig spørsmål te dæ")
+        print(f"{spillerNavn}: Ait bet ka e det? ")
+        print("Brian: Trump eller Biden")
+        presValg = input()
+        if presValg == "Trump" or presValg == "trump":
+            self.valg4 = True
+            print("Brian: Jeg visste visste du ikke var en  ")
+        elif presValg == "Biden" or presValg == "bide":
+            self.valg4 = False
+            print("Brian: Din jævla kommunist faan ")
+        else:
+            self.valg4 = False
+            print(f"Virkelig??")
 
     def braEnding(self):
+        print("")
+        print("(Du tenker til deg selv at Brian var en snillere elev enn du trodde.)")
+        print("(Brian kommer senere under oppsyn av politiet for drap.)")
+        print("(Du tenker. Han var jo så snill det er jo umulig at han kunne gjort noe sånt)")
+        print("(Han flytter og kommer aldri tilbake.)")
         print("")
         print("Du fikk ending 5, den brae endingen til Brian, gratulerer! Det er totalt 6 forskjellige endings i spillet.")
 
     def dårligEnding(self):
+        print("")
+        print("(Brian virket fortsatt ganske hyggelig etter deres tidligere møte og innviterte deg ut å drikke)")
+        print("(Dere drakk ekstremt mye så du klarer ikke komme deg hjem.)")
+        print("Brian: Du kan overnatte hos meg jeg har en ekstra sofa og bor rett borti her.")
+        print("(Du går inn og ser at hele leiligheten er dekt i plastfolie. Du tenker ikke mye over det og han ber deg ta en plass på sofaen.)")
+        print("(Brian går på badet og kommer ut noen minutter senere i full regndrakt og en øks.)")
+        print(f"Brian: {spillerNavn} din jævla kommunist du fortjene å dø")
+        print("(Han løper mot deg og dreper deg med øksen)")
         print("")
         print("Du fikk ending 6, den dårlige endingen til Brian. Det er totalt 6 forskjellige endings i spillet.")
 
@@ -275,6 +323,7 @@ if valg1 == "Olivia":
     Olivia.førsteSpørsmål()
     list.remove("Olivia")
     print(f"Hvem vil du snakke med nå? {list[0]} eller {list[1]}? ")
+    nestValg1=""
     while i==0:
         nesteValg1=input()
         if nesteValg1==list[0]:
@@ -299,7 +348,9 @@ elif valg1 == "Brian":
         nesteValg1=input()
         if nesteValg1==list[1]:
             break
-        elif nesteValg1 != list[0]:
+        elif nesteValg1 == list[0]:
+            break
+        else:
             print("Navnet er ikke gyldig, prøv på nytt.")
     if nesteValg1 == "Olivia":
         Olivia.førsteSpørsmål()
@@ -317,7 +368,9 @@ elif valg1 == "Kat":
         nesteValg1=input()
         if nesteValg1==list[1]:
             break
-        elif nesteValg1 != list[0]:
+        elif nesteValg1 == list[0]:
+            break
+        else:
             print("Navnet er ikke gyldig, prøv på nytt.")
     if nesteValg1 == "Brian":
         Brian.førsteSpørsmål()
